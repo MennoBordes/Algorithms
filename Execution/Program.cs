fd001caad041119543c4a0430d78585c282fee32
Skillerde6de <0948157@hr.nl>
2019-01-17 21:01:07 +0100

    insertion_sort working

--- Execution/Program.cs
+++ Execution/Program.cs
@@ -7,8 +7,9 @@ namespace execution
   {
     static void Main(string[] args)
     {
-    
+      // Creating an array that has to be sorted
       int[] arr = new int[] { 9, 8, 7, 9, 8, 3, 5, 8, 6, 5, 4, 3, 2, 1 };
+      // Calling the sorting function
       var result = array_example.insertion_sort(arr);
     }
   }
--- Execution/array_example.cs
+++ Execution/array_example.cs
@@ -6,19 +6,27 @@ namespace execution
   {
     public static int[] insertion_sort(int[] A)
     {
-      Console.WriteLine(string.Join(",", A));
+      Console.WriteLine("The order before sorting: " + string.Join(",", A));
+      // Loop through for the amount of entries in the table
       for (int j = 1; j < A.Length; j++)
       {
+        // Set key to the current value A
         var key = A[j];
+        // Set compare to the index of the value before key
         int compare = j - 1;
+        // Keep looping until either
+        //  1: compare is below 0
+        //  2: the value of compare is smaller than the value of the key
         while (compare >= 0 && A[compare] > key)
         {
+          // Move the smaller value to the end of the array
           A[compare + 1] = A[compare];
           compare = compare - 1;
         }
+        // Insert the value of key in the location above compare
         A[compare + 1] = key;
       }
-      Console.WriteLine(string.Join(",", A));
+      Console.WriteLine("The order after sorting: " + string.Join(",", A));
       return A;
     }
   }
