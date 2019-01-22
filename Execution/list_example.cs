d609e13b4ca454e7fe7a55547ada2d79fe395a1b
Skillerde6de <mennobordes@hetnet.nl>
2019-01-22 12:08:03 +0100

    list delete functional

--- Execution/list_example.cs
+++ Execution/list_example.cs
@@ -34,9 +34,35 @@ namespace lists
       List_insert(list, 2);
       List_insert(list, 8);
       List_insert(list, 6);
-      var res = List_search(list, 1);       // Search for the node with a given value
-      Console.WriteLine(res.Value);
-
+      // var res = List_search(list, 1);       // Search for the node with a given value
+      // Console.WriteLine(res.Value);
+      List_delete(list, 15);
+    }
+    public static void List_delete(SortedLinkedList<int> list, int toDelete)
+    {
+      if (list.Start == null || list.Start.Value > toDelete)
+      {
+        // The given value is not in the list
+        return;
+      }
+      else if (list.Start.Value == toDelete)
+      {
+        // The given value is the first in the list
+        list.Start = list.Start.Next;
+        return;
+      }
+      var temp = list.Start;
+      while (temp.Next != null && temp.Next.Value <= toDelete)
+      {
+        if (temp.Next.Value == toDelete)
+        {
+          // setting the next node to the current node
+          temp.Next = temp.Next.Next;
+          return;
+        }
+        temp = temp.Next;
+      }
+      return;
     }
     public static Node<int> List_search(SortedLinkedList<int> list, int toFind)
     {
