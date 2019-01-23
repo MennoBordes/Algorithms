b3bec22096241515e6ef85ef171da34cb22d7f51
Skillerde6de <mennobordes@hetnet.nl>
2019-01-23 17:55:06 +0100

    Queue example

--- Execution/Program.cs
+++ Execution/Program.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using Arrays;
 using lists;
 using stacks;
+using Ques;
 
 namespace execution
 {
@@ -16,7 +17,8 @@ namespace execution
       // array_example.callable();             // array example call
       // single_list_example.callable();       // single linked list call
       // double_list_example.callable();       // double linked list call
-      stack_example.callable();             // stack call   
+      // stack_example.callable();             // stack call   
+      Queue_example.callable();             // Queue call
     }
   }
 }
